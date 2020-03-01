e5ba8e84e0ac76394515d449ef9427da7a799535
WillArevalo <willarevalo.developer@gmail.com>
2020-02-29 20:06:27 -0500

    add two probabilities: three and straight

--- listas.py
+++
@@ -1,7 +0,0 @@
-def liston():
-    lista = [1,3,4,5,1,3,3]
-
-    return lista.count()
-
-if __name__ == "__main__":
-    liston()
--- simulation_decks.py
+++ simulation_decks.py
@@ -3,7 +3,7 @@ import random
 import collections
 
 DECKS = ['spades', 'hearts', 'diamonds', 'clubs']
-VALUES = ['as', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k']
+VALUES = [i for i in range(1,14)]
 
 def create_deck():
     """This function create a deck"""
@@ -11,7 +11,6 @@ def create_deck():
     for deck in DECKS:
         for value in VALUES:
             decks.append((deck, value))
-
     return decks
 
 def get_hand(decks, len_hand):
@@ -19,6 +18,10 @@ def get_hand(decks, len_hand):
     hand = random.sample(decks, len_hand)
 
     return hand
+
+def checkConsecutive(l):
+    """Check if is a straight"""
+    return sorted(l) == list(range(min(l), max(l)+1)) 
 
 def main(len_hand, times):
     """get the probabilities of the hands"""
@@ -30,6 +33,8 @@ def main(len_hand, times):
         hands.append(hand)
     
     pairs = 0
+    three_of_a_kind = 0
+    straight = 0
     for hand in hands:
         values = [card[1] for card in hand]
         counter = dict(collections.Counter(values))
@@ -37,9 +42,20 @@ def main(len_hand, times):
             if val == 2:
                 pairs += 1
                 break
+            elif val == 3:
+                three_of_a_kind += 1
+                break
+        if checkConsecutive(values):
+            straight += 1
 
     pair_probability = pairs / times
     print(f'The probability of get a pair in one hand of {len_hand} cards is {pair_probability}')
+    
+    three_probability = three_of_a_kind / times
+    print(f'The probability of get a three of a kind in one hand of {len_hand} cards is {three_probability}')
+
+    straight_probability = straight / times
+    print(f'The probability of get a straight in one hand of {len_hand} cards is {straight_probability}')
 
 if __name__ == '__main__':
     try:
